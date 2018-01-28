a2ba0982fd93acda7e79ea3506b8bc43ec8cbfa7
pp <pp@pp-X550CL>
2018-01-28 11:46:36 +0530

    Adding new project

---
+++ Mapping String List in Collection Mapping using xml/src/com/pravin/hibernate/StoreData.java
@@ -0,0 +1,38 @@
+package com.pravin.hibernate;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class StoreData {
+    public static void main(String[] args) {
+	Session session = new Configuration().configure("hibernate.cfg.xml")
+		.buildSessionFactory()
+		.openSession();
+	Transaction transaction = session.beginTransaction();
+	ArrayList<String> list1 = new ArrayList<String>();
+	list1.add("java is a programming language");
+	list1.add("java is a platform");
+
+	ArrayList<String> list2 = new ArrayList<String>();
+	list2.add("Servlet is an Interface");
+	list2.add("Servlet is an API");
+
+	Question question1 = new Question();
+	question1.setQname("What is Java?");
+	question1.setAnswers(list1);
+
+	Question question2 = new Question();
+	question2.setQname("What is Servlet?");
+	question2.setAnswers(list2);
+
+	session.persist(question1);
+	session.persist(question2);
+
+	transaction.commit();
+	session.close();
+	System.out.println("Success!");
+    }
+}
